bf14aff25e70a84eadaba11afe335944055f8172
Akash Agrawal <akashagrawal0358@gmail.com>
2023-10-07 17:03:00 +0530

    Finally Completed ToDoList

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,10 +1,20 @@
 import React from 'react'
 
 const ToDoList = (props) => {
+
     return (
         <>
             <div className='todolist'>
-                <i class="fa fa-times" />
+
+                {/* click occurs here but function is defined in parent component 
+                    onSelect calls the function in parent component 
+                */}
+                <i className="fa fa-times" onClick={() => {
+                    // props.id gives the id of item that to be deleted
+                    props.onSelect(props.id)
+                     
+                }} />
+
                 <li> {props.item} </li>
             </div>
         </>
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,13 +3,13 @@ import ToDoList from "./ToDoList";
 
 const Todo = () => {
 
-    const [item, setItem] = useState();
+    const [item, setItem] = useState("");
 
     // list array contains all the items 
     const [list, setList] = useState([]);
 
     const AddItem = () => {
-
+        
         setList((oldItems) => {
             //  add all the old items present in array then add new item 
             return [...oldItems, item]
@@ -18,31 +18,48 @@ const Todo = () => {
         // Remove the previous item from input
         setItem("");
     }
+
+    const deleteItem = (id)=>{
+        
+        setList((oldItems)=>{
+            return oldItems.filter((arrElem , index)=>{
+                  return index !== id ;
+            })
+        })
+        console.log("deleted");
+    }
+
+
     return (
         <>
             <div className="main_div">
 
-                <div class=" container center_div" >
-                    <div class="card text-center rounded-2 card_div">
-                        <div class="card-header card-heading">
+                <div className=" container center_div" >
+                    <div className="card text-center rounded-2 card_div">
+                        <div className="card-header card-heading">
                             <h1 >To Do List</h1>
                         </div>
-                        <div class="card-body">
-                            <h4 class="card-title ">Add Items in List...</h4>
-                            <p class="card-text">
+                        <div className="card-body ">
+                            <h4 className="card-title ">Add Items in List...</h4>
+                            <p className="card-text">
                                 <input className="input" type="text" id="itemInput"
-                                    onChange={(e) => setItem(e.target.value)} value={item} />
+                                    onChange={(e) =>  setItem(e.target.value) } value={item} />
                                 <button className="btnn" onClick={AddItem}>+</button>
                             </p>
                             <ol>
                                 {  // Iterate over the list array and return all items
-                                    list.map((curElem) => {
-                                        return <ToDoList item={curElem}/>
+                                    list.map((curElem , index) => {
+
+                                        // key is used to identify a particular item
+                                        return <ToDoList key={index} 
+                                           id={index} item={curElem}
+                                           onSelect={deleteItem}
+                                        />
                                     })
                                 }
                             </ol>
                         </div>
-                        <div class="card-footer text-body-secondary">
+                        <div className="card-footer text-body-secondary">
                             @copyright Agrawal
                         </div>
 
